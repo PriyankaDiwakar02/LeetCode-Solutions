af57e7c04d43ba5dcad5a3c6f96175bef8e4f753
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-08-09 22:30:23 +0530

    Time: 175 ms (54.06%), Space: 19.5 MB (93.18%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,10 +1,28 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
-        n,prf = len(piles), [*accumulate(piles, initial=0)]
-        @cache 
-        def dfs(p, lb, m): return (0  if  lb >= n  else
-            min(dfs(1-p, lb+x, max(x,m))  for x in range(1, 2*m+1) if lb+x <= n)  if p  else
-            max(dfs(1-p, lb+x, max(x,m)) +prf[lb+x]-prf[lb] for x in range(1, 2*m+1) if lb+x <= n)
-        )
-        return dfs(0, 0, 1)
-        
+        n = len(piles)
+
+        # dp[i][m] = maximum stones the current player can collect
+        # starting at index i with the current value of M equal to m
+        dp = [[0] * (n + 1) for _ in range(n)]
+
+        # suffix[i] = total stones from piles[i] to piles[n - 1]
+        suffix = [piles[-1]] * n
+
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        for i in range(n - 1, -1, -1):
+            for m in range(n, 0, -1):
+                # The current player can take every remaining pile
+                if i + 2 * m >= n:
+                    dp[i][m] = suffix[i]
+                else:
+                    for x in range(1, 2 * m + 1):
+                        opponent = dp[i + x][max(m, x)]
+                        dp[i][m] = max(
+                            dp[i][m],
+                            suffix[i] - opponent
+                        )
+
+        return dp[0][1]
