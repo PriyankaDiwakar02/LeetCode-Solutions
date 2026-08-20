21cdc717f794614a8147196b8234b33a2a96bdb8
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-08-20 20:38:48 +0530

    Time: 15 ms (15.54%), Space: 19.7 MB (7.33%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int solve(vector<int>& piles, int i, int j)
-    {
-        if(i > j) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int takei = piles[i] + min(solve(piles, i + 2, j), solve(piles, i + 1, j - 1));
-        int takej = piles[j] + min(solve(piles, i, j - 2),solve(piles, i + 1, j - 1));
-
-        return dp[i][j] = max(takei, takej);
+    int f(vector<int>& p, int i, int j, vector<vector<int>>& dp) {
+        if (i == j) return p[i];
+        if (dp[i][j] != INT_MIN) return dp[i][j];
+        return dp[i][j] = max(p[i] - f(p, i + 1, j, dp),
+                              p[j] - f(p, i, j - 1, dp));
     }
-
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        dp.assign(n, vector<int>(n, -1));
-        int sum = accumulate(begin(piles), end(piles), 0);
-        int alice = solve(piles, 0, n - 1);
-        return alice > sum / 2;
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        return f(piles, 0, n - 1, dp) > 0;
     }
 };
