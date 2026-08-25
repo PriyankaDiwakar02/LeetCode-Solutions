c36e0b4717e8730b5b50d01958dd666ef02cc578
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-08-25 12:02:13 +0530

    Time: 0 ms (100%), Space: 10.9 MB (74.71%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,28 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        if(root == nullptr) return {};
-        vector<int> ans;
-        stack<TreeNode*> myStack;
-        myStack.push(root);
-        while(!myStack.empty()){
-            TreeNode* temp = myStack.top();
-            ans.push_back(temp->val);
-            myStack.pop();
-            if(temp->right != nullptr) myStack.push(temp->right);
-            if(temp->left != nullptr) myStack.push(temp->left);
-        }
-        return ans;
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+    
+    void dfs(TreeNode* node, vector<int>& result) {
+        if (!node) return;
+        result.push_back(node->val); // Visit root
+        dfs(node->left, result);     // Traverse left subtree
+        dfs(node->right, result);    // Traverse right subtree
     }
 };
