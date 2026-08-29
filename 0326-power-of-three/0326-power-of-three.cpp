4ba533fa76fb18c631882d27aec12862bd864a60
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-08-29 18:19:30 +0530

    Time: 0 ms (100%), Space: 9.1 MB (9.94%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        double x = log(n) / log(3.0);
+
+        return abs(x - round(x)) < 1e-10;
+    }
+};
