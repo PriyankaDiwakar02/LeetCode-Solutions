658240ab6b0bd6489c840d70883af30660d12bbf
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-09-01 21:02:33 +0530

    Time: 0 ms (100%), Space: 8.3 MB (50.26%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int squareSum(int n){
-        int ans=0;
-        while(n>0){
-            ans=ans+pow((n%10),2);
-            n=n/10;
-        }
-        return ans;
-    }
     bool isHappy(int n) {
-        unordered_set<int> seen;
-        while(n!=1){
-            if(seen.count(n)==1){
-                return false;
-            }else{
-                seen.insert(n);
+        // Create a set...
+        set<int> hset;
+        while(hset.count(n) == 0) {
+            // If total is equal to 1 return true.
+            if(n == 1)
+                return true;
+            // Insert the current number in hset...
+            hset.insert(n);
+            // Initialize the total...
+            int total=0;
+            // Create a while loop...
+            while(n) {
+                // Process to get happy number...
+                // We use division and modulus operators to repeatedly take digits off the number until none remain...
+                // Then squaring each removed digit and adding them together.
+                total += (n % 10) * (n % 10);
+                n /= 10;
+                // Each new converted number must not had occurred before...
             }
-            n=squareSum(n);
+            // Insert the current number into the set s...
+            // Replace the current number with total of the square of its digits.
+            n = total;
         }
-        return true;
+        // If current number is already in the HashSet, that means we're in a cycle and we should return false..
+        return false;
     }
 };
