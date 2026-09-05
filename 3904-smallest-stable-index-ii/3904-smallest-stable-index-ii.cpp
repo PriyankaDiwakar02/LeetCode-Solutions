3d1cf3ad60b7323d5457e71de6b6dfe455bb4bb1
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-09-05 22:08:39 +0530

    Time: 0 ms (100%), Space: 193.5 MB (100%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-                int minVal = INT_MAX;
-        int maxVal = INT_MIN;
+        int n = nums.size();
 
-        vector<int> pref(nums.size());
-        vector<int> suff(nums.size());
+        int ansIdx = 0;         // index we're currently testing as the answer
+        int globalMax = INT_MIN;          // biggest number seen anywhere so far
+        int ansMax = INT_MIN;   // biggest number up to ansIdx
 
-        for (int i = 0; i < nums.size(); i++) {
-            maxVal = max(maxVal, nums[i]);
-            pref[i] = maxVal;
+        for(int i = 0; i < n; i++){
+            globalMax = max(globalMax, nums[i]);
+
+            // only update the candidate's max while we're still inside its prefix
+            if(i == ansIdx)
+                ansMax = max(ansMax, nums[i]);
+
+            // this number is below the allowed floor, jump past it
+            if(nums[i] < ansMax - k){
+                ansIdx = i + 1;
+                ansMax = globalMax;
+            }
         }
 
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            minVal = min(minVal, nums[i]);
-            suff[i] = minVal;
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (pref[i] - suff[i] <= k)
-                return i;
-        }
-
-        return -1;
-
+        return ansIdx < n ? ansIdx : -1;
     }
 };
