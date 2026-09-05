316c362e006737b27951e1f52076fa754607961a
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-09-05 22:11:14 +0530

    Time: 3 ms (16.92%), Space: 7.9 MB (72.05%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,27 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        if(n == 2){
-            if(isBadVersion(1))
-            return 1;
-            else
-            return 2;
+        int left=1,mid;
+        while(left<=n){
+            mid=left+(n-left)/2;
+            if(isBadVersion(mid)){
+                n=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
         }
-
-        long long int l = 1 , r = n , mid;
-        while(l<=r){
-            mid = (l+r)/2;
-            if(mid == 1)
-            return 1;
-            else if(mid == n)
-            return n;
-            else if(isBadVersion(mid) && !isBadVersion(mid-1))
-            return mid;
-            else if(isBadVersion(mid))
-            r = mid-1;
-            else
-            l = mid+1;
-        }
-        return -1;
+        return left;
     }
 };
