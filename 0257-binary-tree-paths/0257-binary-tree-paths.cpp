6adf3ca578916caef1f89815b3c7fe201d619966
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-09-09 21:34:38 +0530

    Time: 0 ms (100%), Space: 18.6 MB (7.46%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,19 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void helper(TreeNode* root,vector<string>& answer,string curr){
-        if(!root)return;
-        if(root->left||root->right)curr+=(to_string(root->val)+"->");
-        else{
-            curr+=(to_string(root->val));
-            answer.push_back(curr);
+    void dfs(TreeNode* root, string temp, vector<string>& ans){
+        // bc
+        if(!root) return;
+        if(!root->left && !root->right){
+            temp+= to_string(root->val);
+            ans.push_back(temp);
+
         }
-        helper(root->left,answer,curr);
-        helper(root->right,answer,curr);  
+
+        //task
+        temp+= to_string(root->val) + "->";
+        // rr
+        dfs(root->left, temp, ans);
+        dfs(root->right, temp, ans);
+
+        
     }
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> answer;
-        string curr="";
-        helper(root,answer,curr);
-        return answer;
+        vector<string> ans;
+        string temp="";
+        dfs(root, temp, ans);
+        return ans;
+
+        
     }
 };
