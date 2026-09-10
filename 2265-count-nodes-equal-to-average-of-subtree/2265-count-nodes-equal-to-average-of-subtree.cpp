ec785eb4f9481df994a8d90de5ac6c7000f7ba39
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-09-10 21:39:28 +0530

    Time: 7 ms (54.96%), Space: 16 MB (9.02%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,32 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    using int2=pair<int, int>;
 public:
-    int count=0;
-    int2 postOrder(TreeNode* node){
-        if (node==NULL) return {0, 0};
-        auto [sumL, iL]=postOrder(node->left);
-        auto [sumR, iR]=postOrder(node->right);
-        int sum=node->val, i=1;
-        sum+=(sumL+sumR);
-        i+=(iL+iR);
-        if (sum/i==node->val) count++;
-    //    cout<<node->val<<" n="<<i<<" sum="<<sum<<endl;
-        return {sum, i};
-    }
-
-    int averageOfSubtree(TreeNode* root) {
-        postOrder(root);
-        return count;
+    struct P {
+        int s{}, c{};
+        inline void add(P x) {
+            s += x.s;
+            c += x.c;
+        }
+    };
+    int averageOfSubtree(TreeNode* x) {
+        int r = 0;
+        [&r](this auto&& f, TreeNode* x) -> P {
+            P p{};
+            if (x) {
+                p.add(P{x->val, 1});
+                p.add(f(x->left));
+                p.add(f(x->right));
+                r += ((p.s / p.c) == x->val);
+            }
+            return p;
+        }(x);
+        return r;
     }
 };
