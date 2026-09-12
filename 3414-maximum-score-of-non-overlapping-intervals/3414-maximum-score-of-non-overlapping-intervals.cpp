1d6c9d99ec720360c3dea5090fa66ab92c64311c
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-09-12 18:07:05 +0530

    Time: 746 ms (70.65%), Space: 272.1 MB (71.74%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -1,77 +1,28 @@
 class Solution {
 public:
-    struct State {
-        long long score = 0;
-        vector<int> idx;
-    };
-
-    bool better(const State& a, const State& b) {
-        if (a.score != b.score)
-            return a.score > b.score;
-
-        return a.idx < b.idx;
-    }
-
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
         int n = intervals.size();
+        vector<int> order(n);
+        iota(order.begin(), order.end(), 0);
+        sort(order.begin(), order.end(), [&](int a, int b) { return intervals[a][1] < intervals[b][1]; });
+        vector<int> rights(n);
+        for (int p = 0; p < n; ++p) rights[p] = intervals[order[p]][1];
 
-        // {start, end, weight, original_index}
-        vector<array<int, 4>> a;
-
-        for (int i = 0; i < n; i++) {
-            a.push_back({
-                intervals[i][0],
-                intervals[i][1],
-                intervals[i][2],
-                i
-            });
-        }
-
-        // Sort by ending point
-        sort(a.begin(), a.end(), [](auto& x, auto& y) {
-            return x[1] < y[1];
-        });
-
-        vector<int> ends(n);
-
-        for (int i = 0; i < n; i++)
-            ends[i] = a[i][1];
-
-        // Find previous non-overlapping interval
-        vector<int> prev(n);
-
-        for (int i = 0; i < n; i++) {
-            prev[i] =
-                lower_bound(ends.begin(), ends.end(), a[i][0])
-                - ends.begin();
-        }
-
-        // dp[i][k] = best answer using first i intervals
-        // while selecting at most k intervals
-        vector<vector<State>> dp(n + 1, vector<State>(5));
-
-        for (int i = 1; i <= n; i++) {
-
-            // Skip current interval
-            for (int k = 1; k <= 4; k++)
-                dp[i][k] = dp[i - 1][k];
-
-            // Take current interval
-            for (int k = 1; k <= 4; k++) {
-
-                State take = dp[prev[i - 1]][k - 1];
-
-                take.score += a[i - 1][2];
-
-                take.idx.push_back(a[i - 1][3]);
-
-                sort(take.idx.begin(), take.idx.end());
-
-                if (better(take, dp[i][k]))
-                    dp[i][k] = take;
+        using State = pair<long long, vector<int>>;
+        vector<State> prev(n + 1, {0, {}});  // k = 0: nothing picked
+        for (int k = 0; k < 4; ++k) {
+            vector<State> cur(n + 1, {0, {}});
+            for (int p = 1; p <= n; ++p) {
+                int i = order[p - 1];  // take next interval
+                int l = intervals[i][0], w = intervals[i][2];
+                int j = lower_bound(rights.begin(), rights.end(), l) - rights.begin();  // intervals ending before l
+                State take = prev[j];
+                take.first -= w;  // scores kept negative
+                take.second.insert(upper_bound(take.second.begin(), take.second.end(), i), i);
+                cur[p] = min(take, cur[p - 1]);  // min = best score, then lex smallest
             }
+            prev = move(cur);
         }
-
-        return dp[n][4].idx;
+        return prev[n].second;
     }
 };
