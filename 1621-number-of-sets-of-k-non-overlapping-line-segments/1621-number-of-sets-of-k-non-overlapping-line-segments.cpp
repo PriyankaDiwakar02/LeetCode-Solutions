c1bc458c248742d0bba44d1ef3dbb9fcb92b205a
PriyankaDiwakar02 <pdiwakar116@gmail.com>
2026-09-16 21:19:50 +0530

    Time: 30 ms (66.36%), Space: 10.1 MB (72.9%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,36 +1,29 @@
 class Solution {
-    int power(int a, int b, const int mod){
-        int ans = 1;
-        while(b){
-            if(b & 1)ans = (ans * 1ll * a) % mod;
-            a = (a * 1ll * a) % mod;
-            b >>= 1;
-        }
-        return ans;
-    }
 public:
     int numberOfSets(int n, int k) {
-        vector<int> f(n+1), inv(n+1);
-        f[0] = f[1] = 1;
-        const int mod = 1e9 + 7;
-        for(int i = 2; i <= n; i++){
-            f[i] = (i * 1ll * f[i - 1]) % mod;
+        const int MOD = 1e9 + 7;
+
+        vector<long long> dp(n, 1);
+        vector<long long> prefixSums(n + 1, 0);
+
+        for (int j = 0; j < n; j++) {
+            prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
         }
-        for(int i = 0; i <= n; i++){
-            inv[i] = power(f[i], mod - 2 , mod);
+
+        for (int i = 0; i < k; i++) {
+            dp[0] = 0;
+
+            for (int j = 1; j < n; j++) {
+                dp[j] = (dp[j - 1] + prefixSums[j]) % MOD;
+            }
+
+            prefixSums[0] = 0;
+
+            for (int j = 0; j < n; j++) {
+                prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+            }
         }
-        auto ncr = [&](int n, int r){
-            if(r < 0 || r > n)return 0;
-            int num = f[n];
-            int den = (inv[r] * 1ll * inv[n - r]) % mod;
-            int ans = (num * 1ll * den) % mod;
-            return ans;
-        };
-        int ans = 0;
-        for(int r = k + 1; r <= k<<1; r++){
-            int x = r - k - 1;
-            ans = (ans + (ncr(r - x - 2, x) * 1ll * ncr(n, r)) % mod) % mod;
-        }
-        return ans;
+
+        return dp[n - 1];
     }
 };
